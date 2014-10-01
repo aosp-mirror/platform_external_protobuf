bcf45045f229edab8c2589c1f561e7b7495a763b
Brian Duff <bduff@google.com>
2014-10-01 13:33:40 -0700

    Add clone() method support for nano.
    Upstreamed from Another Place (cr/57247854).
    
    Change-Id: I2aaf59544c0f5ae21a51891d8a5eeda1dc722c90

--- src/google/protobuf/compiler/javanano/javanano_primitive_field.h
+++ src/google/protobuf/compiler/javanano/javanano_primitive_field.h
@@ -108,6 +108,7 @@ class RepeatedPrimitiveFieldGenerator : public FieldGenerator {
   void GenerateSerializedSizeCode(io::Printer* printer) const;
   void GenerateEqualsCode(io::Printer* printer) const;
   void GenerateHashCodeCode(io::Printer* printer) const;
+  void GenerateFixClonedCode(io::Printer* printer) const;
 
  private:
   void GenerateRepeatedDataSizeCode(io::Printer* printer) const;
